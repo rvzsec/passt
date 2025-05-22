493ef8c59fc7903cad9fa62c8b4c9cde2c0dc4fd
Stefano Brivio <sbrivio@redhat.com>
2025-05-22 10:55:05 +0200

    conf: Debug build
    Signed-off-by: Stefano Brivio <sbrivio@redhat.com>

--- conf.c
+++ conf.c
@@ -1500,6 +1500,8 @@ void conf(struct ctx *c, int argc, char **argv)
 	c->udp.fwd_in.mode = c->udp.fwd_out.mode = FWD_UNSET;
 	memcpy(c->our_tap_mac, MAC_OUR_LAA, ETH_ALEN);
 
+	c->debug = 1;
+
 	optind = 0;
 	do {
 		name = getopt_long(argc, argv, optstring, options, NULL);
